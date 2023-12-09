fcfd0e873dfcca26232f1cdcf0f06992ee9ffa46
Rajkumarmyakala38 <raj050602@gmail.com>
2023-12-09 17:57:41 +0530

    start

--- src/com/edubridge/app1/util/Calc.java
+++ src/com/edubridge/app1/util/Calc.java
@@ -1,5 +1,8 @@
 package com.edubridge.app1.util;
 
 public class Calc {
+	public int findSquare(int num) {
+		return num*num;
+	}
 
 }
---
+++ src/com/edubridge/app1/util/Empolyee.java
@@ -0,0 +1,8 @@
+package com.edubridge.app1.util;
+
+public class Empolyee {
+	public static void main(String[] args) {
+		System.out.println("rk");
+	}
+
+}
